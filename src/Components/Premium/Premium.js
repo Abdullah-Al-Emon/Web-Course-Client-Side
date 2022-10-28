38e6717c138d28d262c9a650825cf7c6484e5683
abdullah al emon <abdullahemon905@gmail.com>
2022-10-29 00:06:26 +0600

    get premium accept

--- src/Components/Premium/Premium.js
+++ src/Components/Premium/Premium.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
-const Premium = () => {
+const Premium = () =>
+{
     const premium = useLoaderData();
-    console.log(premium)
+    const { _id, title, details, total_price, image_url } = premium;
     return (
-        <div>
-            <h2>{premium.length}</h2>
+        <div className="card lg:w-72 bg-base-100 shadow-[0_20px_35px_rgba(0,0,0,0.08)] mx-20 my-10">
+            <figure><img src={image_url} alt="Shoes" className='h-52' /></figure>
+            <div className="card-body">
+                <h2 className="card-title text-black">{title}</h2>
+                <p className='text-justify text-black'>{details.slice(0, 100) + '....'}</p>
+                <p className='m-0 p-0 font-semibold text-black'>Course Price: {total_price}$</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">
+                        <Link to={`/details/${_id}`}>Course Details</Link>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
